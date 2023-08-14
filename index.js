1df1e71a1f2f704b0eecad787620cb85c8972bed
RishiChaary <rs3715@srmist.edu.in>
2023-08-14 19:26:47 +0530

    StableBackend02

--- index.js
+++ index.js
@@ -23,66 +23,55 @@ app.listen(process.env.port || 3001 , () => {
 } );
 
 app.put("/SaveFile" , async (req,res)=>{
-    const base64Data = req.body.File.split('base64,')[1];
-    const tmpFilePath = path.join(`./${Math.random().toString(36).substr(7)}.pdf`);
-    var bin = Base64.atob(req.body.File);
-    fs.writeFile(tmpFilePath, base64Data, 'base64', async (error) => {
-        if (error) {
-            throw error;
-        } else {
-            let doc = await pdfjsLib.getDocument(tmpFilePath).promise;
-            let page1 = await doc.getPage(1);
-            let content = await page1.getTextContent();
-            let strings = content.items.map(function(item) {
-                return item.str;
-            });
-
-            let i=0;
-            let finalString = '';
-            for(i=0;i<strings.length;i++){
-                if(strings[i] == ''){
-                    finalString = finalString+" ";
-                }
-                else{
-                    finalString = finalString+strings[i]
-                }
-            }
-            const openai = new OpenAIApi(configuration);
-
-            const history = [];
-
-            const user_input = finalString+" ANALYSE AND RUN A DIFFUCULT RATING OF CANDIDATE RATE OUT OF 10 WITH OVERALL RATING , MAKE THE EVALUVATION STRICT  ON EACH ASPECT BASED ON THIS INFO";
-
-            const messages = [];
-            for (const [input_text, completion_text] of history) {
-            messages.push({ role: "user", content: input_text });
-            messages.push({ role: "assistant", content: completion_text });
-            }
-
-            messages.push({ role: "user", content: user_input });
-
-            try {
-            const completion = await openai.createChatCompletion({
-                model: "gpt-3.5-turbo",
-                messages: messages,
-            });
-
-            const completion_text = completion.data.choices[0].message.content;
-            res.send(completion_text)
-
-            history.push([user_input, completion_text]);
-            } catch (error) {
-            if (error.response) {
-                console.log(error.response.status);
-                console.log(error.response.data);
-            } else {
-                console.log(error.message);
-            }
-        }
-        fs.unlinkSync(tmpFilePath);
-        }
-
+    
+    let doc = await pdfjsLib.getDocument(req.body.File).promise;
+    let page1 = await doc.getPage(1);
+    let content = await page1.getTextContent();
+    let strings = content.items.map(function(item) {
+        return item.str;
     });
+    let i=0;
+    let finalString = '';
+    for(i=0;i<strings.length;i++){
+        if(strings[i] == ''){
+            finalString = finalString+" ";
+        }
+        else{
+            finalString = finalString+strings[i]
+        }
+    }
+    
+    const openai = new OpenAIApi(configuration);
+
+    const history = [];
+
+    const user_input = finalString+" ANALYSE AND RUN A DIFFUCULT RATING OF CANDIDATE RATE OUT OF 10 WITH OVERALL RATING , MAKE THE EVALUVATION STRICT  ON EACH ASPECT BASED ON THIS INFO";
+
+    const messages = [];
+    for (const [input_text, completion_text] of history) {
+    messages.push({ role: "user", content: input_text });
+    messages.push({ role: "assistant", content: completion_text });
+    }
+
+    messages.push({ role: "user", content: user_input });
+
+    try {
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+    });
+
+    const completion_text = completion.data.choices[0].message.content;
+    res.send(completion_text)
+
+    history.push([user_input, completion_text]);
+    } catch (error) {
+    if (error.response) {
+        console.log(error.response.status);
+        console.log(error.response.data);
+    } else {
+        console.log(error.message);
+    }}
 })
 
 app.put("/PutQuerry" , async (req,res)=>{
